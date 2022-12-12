f6d1b73ce1ee08b681839f31abb4c15206cae0e3
Miguel Angel Sanchez Papalotzi <msanchezpapalotzi@gmail.com>
2022-12-11 22:38:43 -0600

    Iniciando Proyeto Final Alternativo

---
+++ src/main/java/com/mycompany/proyectofinal/ProyectoFinal.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.proyectofinal;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ *
+ * @author MigueSkz
+ */
+public class ProyectoFinal {
+
+    public static void main(String[] args) {
+        
+        System.out.println("😢😢😢😢😢😢😢😢😢😢😢😢😢😢😢");
+        BufferedReader bufer =  new BufferedReader(new InputStreamReader(System.in));    
+        
+        System.out.println("Este es un Pograma que Crea un Archivo mediante una Pila... :) ");
+        String entrada;
+        String nombre;
+        int opcion;
+        char respuesta;
+        
+        
+    }
+}
